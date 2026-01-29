9088e5cc2976f194b232a956dd186af96687b253
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 18:53:23 +0000

    Update industry_list _rules.sql

--- src/templates/customer_entities/industry_list _rules.sql
+++ src/templates/customer_entities/industry_list _rules.sql
@@ -1,14 +1,3 @@
-DROP TABLE IF EXISTS customer_industry_list_rules
-
-CREATE TABLE customer_industry_list_rules(
-    [rule] NVARCHAR(50),
-    valid_count INT,
-    invalid_count INT
-);
-
--- Insert the aggregated data into the new table
-INSERT INTO customer_industry_list_rules ([rule], valid_count, invalid_count)
-
 --Industry in industry list
 SELECT 
     'Values in industry list' AS [rule],
@@ -23,4 +12,5 @@ FROM (
         END AS industry_check
     FROM customer
 ) AS industry_validation;
+
 
