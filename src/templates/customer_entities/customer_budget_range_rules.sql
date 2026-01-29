5648bdf82036c9e55422f8453bda538780c3aa1e
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 18:52:01 +0000

    Update customer_budget_range_rules.sql

--- src/templates/customer_entities/customer_budget_range_rules.sql
+++ src/templates/customer_entities/customer_budget_range_rules.sql
@@ -1,23 +1,13 @@
-﻿DROP TABLE IF EXISTS customer_budget_range_rules
-
-CREATE TABLE customer_budget_range_rules(
-    [rule] NVARCHAR(50),
-    valid_count INT,
-    invalid_count INT
-);
-
--- Insert the aggregated data into the new table
-INSERT INTO customer_budget_range_rules ([rule], valid_count, invalid_count)
-
 --Starts with a currency symbol
 SELECT 
     'Starts with Currency Symbol' AS [rule],
     COUNT(CASE WHEN PATINDEX('[$€£¥₹][0-9]%', budget) = 1 THEN 1 END) AS valid,
     COUNT(CASE WHEN PATINDEX('[$€£¥₹][0-9]%', budget) != 1 THEN 1 END) AS invalid
 FROM customer
-UNION
+UNION ALL
 
 --Optional Budget Format
+
 SELECT 
     'Valid Budget Range Format' AS [rule],
     COUNT(CASE 
