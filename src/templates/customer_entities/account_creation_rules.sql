7b79e2745415ed51a9ef391f02d76cdaa7a37e87
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 18:41:47 +0000

    Update account_creation_rules.sql

--- src/templates/customer_entities/account_creation_rules.sql
+++ src/templates/customer_entities/account_creation_rules.sql
@@ -1,22 +1,12 @@
-DROP TABLE IF EXISTS account_creation_rules
-
-CREATE TABLE account_creation_rules(
-    [rule] NVARCHAR(50),
-    valid_count INT,
-    invalid_count INT
-);
-
--- Insert the aggregated data into the new table
-INSERT INTO account_creation_rules ([rule], valid_count, invalid_count)
-
 --SQL has date time, so simply TRY_CAST for date time format, will return nulls if not working, we are very lucky
+--handling dates is painful
 --Date time format
 SELECT 
 	'Date time format' as [rule],
 	COUNT(TRY_CAST(account_creation as datetime)) as valid,
 	COUNT(CASE WHEN TRY_CAST(account_creation as datetime) IS NULL THEN 1 END) as invalid	
 FROM customer
-UNION
+UNION ALL
 --account created from 2015 onwards
 SELECT 
 	'Between 2015 and Current Date' as [rule],
@@ -25,3 +15,4 @@ SELECT
 	COUNT(CASE WHEN TRY_CAST(account_creation as datetime) <'2015' OR   TRY_CAST(account_creation AS datetime) > GETDATE() 
                THEN 1 END) as invalid
 FROM customer
+
