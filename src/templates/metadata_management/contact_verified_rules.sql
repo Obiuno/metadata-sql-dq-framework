af86b1d8c10fcaa92b418d8a7ac10447b42bfa8d
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 19:12:50 +0000

    Update contact_verified_rules.sql

--- src/templates/metadata_management/contact_verified_rules.sql
+++ src/templates/metadata_management/contact_verified_rules.sql
@@ -1,33 +1,24 @@
-DROP TABLE IF EXISTS contact_verified_rules
-
-CREATE TABLE contact_verified_rules(
-    [rule] NVARCHAR(50),
-    valid_count INT,
-    invalid_count INT
-);
-
--- Insert the aggregated data into the new table
-INSERT INTO contact_verified_rules ([rule], valid_count, invalid_count)
-
 --Valid Format
 SELECT 
 	'Valid format' as [rule],
 	COUNT(CASE WHEN TRY_CAST(verified as INT) = 1 OR TRY_CAST(verified as INT) = 0 THEN 1 END) as valid,
 	COUNT(CASE WHEN TRY_CAST(verified as INT) = 1 OR TRY_CAST(verified as INT) = 0 THEN NULL ELSE 1 END) as invalid 
 FROM contact
-UNION
+UNION ALL
 
 --Email present for Verified Contact Rule
+--If a contact claims to be verified (1), they MUST have an email address.
 SELECT 
 	'Email present for verified contact' as [rule],
 	COUNT(CASE WHEN (TRY_CAST(verified as INT) = 1 AND email IS NOT NULL) THEN 1 END) as valid,
 	COUNT(CASE WHEN (TRY_CAST(verified as INT) = 1 AND email IS NULL) THEN 1 END) as invalid 
 FROM contact
-UNION
+UNION ALL
 
---Email is Verified
+--BR - Email is Verified
 SELECT 
 	'Email verified' as [rule],
 	COUNT(CASE WHEN TRY_CAST(verified as INT) =1 THEN 1 END) valid,
 	COUNT(CASE WHEN TRY_CAST(verified as INT) != 1 THEN 1 END) invalid
 FROM contact
+
