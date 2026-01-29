d69095881818c8a08cdcc4f07c72792fdb24f9da
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 18:51:17 +0000

    Update company_name_rules.sql

--- src/templates/customer_entities/company_name_rules.sql
+++ src/templates/customer_entities/company_name_rules.sql
@@ -1,20 +1,11 @@
-CREATE TABLE company_name_rules (
-    [rule] NVARCHAR(50),
-    valid_count INT,
-    invalid_count INT
-);
-
--- Insert the aggregated data into the new table
-INSERT INTO company_name_rules ([rule], valid_count, invalid_count)
-
--- All chacters allowed, redundant rule as it will always accept everything
+-- All characters allowed, redundant rule as it will always accept everything
 -- Name leng >= 2
 SELECT 
 	'Name length >= 2' as [rule],
 	COUNT(CASE WHEN LEN(company_name) >= 2 THEN company_name END) valid,
 	COUNT(CASE WHEN LEN(company_name) < 2 THEN company_name END) invalid
 FROM customer
-UNION
+UNION ALL
 
 -- Name len <=100
 SELECT 
@@ -23,3 +14,4 @@ SELECT
 	COUNT(CASE WHEN LEN(company_name) > 100 THEN company_name END) invalid
 
 FROM customer
+
