aab15c81baa8a43de0ca7610dfa8a7468c64bedd
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 18:37:59 +0000

    Update mobile_and_phone_format.sql

--- src/templates/contact_validation/mobile_and_phone_format.sql
+++ src/templates/contact_validation/mobile_and_phone_format.sql
@@ -3,13 +3,9 @@ SELECT
     COUNT(CASE WHEN mobile LIKE '+[0-9]%' THEN 1 END) AS valid,
     COUNT(CASE WHEN  mobile NOT LIKE '+[0-9]%' THEN 1 END) AS invalid
 FROM contact
-
+UNION ALL
 SELECT 
     'Phone Number Format' AS [rule],
     COUNT(CASE WHEN phone LIKE '+[0-9]%' THEN 1 END) AS valid,
     COUNT(CASE WHEN  phone NOT LIKE '+[0-9]%' THEN 1 END) AS invalid
 FROM contact
-
-SELECT * FROM contact
-
-
