6d6d5ea9be553f5201a868ddb9915aa41cbc7341
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 18:54:45 +0000

    Update contact_creation_date_rules.sql

--- src/templates/metadata_management/contact_creation_date_rules.sql
+++ src/templates/metadata_management/contact_creation_date_rules.sql
@@ -1,21 +1,10 @@
-DROP TABLE IF EXISTS contact_creation_date_rules
-
-CREATE TABLE contact_creation_date_rules(
-    [rule] NVARCHAR(50),
-    valid_count INT,
-    invalid_count INT
-);
-
--- Insert the aggregated data into the new table
-INSERT INTO contact_creation_date_rules ([rule], valid_count, invalid_count)
-
 --Date time format
 SELECT 
 	'Date time format' as [rule],
 	COUNT(TRY_CAST(creation_date as datetime)) as valid,
 	COUNT(CASE WHEN TRY_CAST(creation_date as datetime) IS NULL THEN 1 END) as invalid	
 FROM contact
-UNION
+UNION ALL
 
 --Year range rule
 SELECT 
@@ -23,3 +12,4 @@ SELECT
 	COUNT(CASE WHEN TRY_CAST(creation_date as datetime) >='2015' AND TRY_CAST(creation_date as datetime) <=GETDATE() THEN 1 END) as valid,
 	COUNT(CASE WHEN TRY_CAST(creation_date as datetime) <'2015' OR TRY_CAST(creation_date as datetime) > GETDATE() THEN 1 END) as invalid
 FROM contact
+
