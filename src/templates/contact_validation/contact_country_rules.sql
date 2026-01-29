59f973a89feb8d86b127796616613c1bf1043cb9
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 18:35:20 +0000

    Update contact_country_rules.sql

--- src/templates/contact_validation/contact_country_rules.sql
+++ src/templates/contact_validation/contact_country_rules.sql
@@ -1,14 +1,3 @@
-DROP TABLE IF EXISTS contact_country_rules
-
-CREATE TABLE contact_country_rules(
-    [rule] NVARCHAR(50),
-    valid_count INT,
-    invalid_count INT
-);
-
--- Insert the aggregated data into the new table
-INSERT INTO contact_country_rules ([rule], valid_count, invalid_count)
-
 --Country in country list
 SELECT 
     'Country in country list' AS [rule],
@@ -22,4 +11,5 @@ FROM (
             ELSE 'Invalid'
         END AS country_check
     FROM contact
+
 ) AS country_validation
