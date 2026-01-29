2e6cbd8fe54efce30feb3b4e1d9abc27b2bf56f1
Obiuno <70383879+Obiuno@users.noreply.github.com>
2026-01-29 18:05:55 +0000

    added mdm rules

---
+++ src/templates/metadata_management/contact_marketing_rules.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS contact_marketing_rules
+
+CREATE TABLE contact_marketing_rules(
+    [rule] NVARCHAR(50),
+    valid_count INT,
+    invalid_count INT
+);
+
+-- Insert the aggregated data into the new table
+INSERT INTO contact_marketing_rules ([rule], valid_count, invalid_count)
+
+--Valid Format
+SELECT 
+	'Valid format' as [rule],
+	COUNT(CASE WHEN TRY_CAST(marketing as INT) = 1 OR TRY_CAST(marketing as INT) = 0 THEN 1 END) as valid,
+	COUNT(CASE WHEN TRY_CAST(marketing as INT) = 1 OR TRY_CAST(marketing as INT) = 0 THEN NULL ELSE 1 END) as invalid 
+FROM contact
+UNION
+
+--Email present for Verified Contact Rule
+SELECT 
+	'Email present for verified contact' as [rule],
+	COUNT(CASE WHEN (TRY_CAST(marketing as INT) = 1 AND email IS NOT NULL) THEN 1 END) as valid,
+	COUNT(CASE WHEN (TRY_CAST(marketing as INT) = 1 AND email IS NULL) THEN 1 END) as invalid 
+FROM contact
---
+++ src/templates/metadata_management/contact_verified_rules.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS contact_verified_rules
+
+CREATE TABLE contact_verified_rules(
+    [rule] NVARCHAR(50),
+    valid_count INT,
+    invalid_count INT
+);
+
+-- Insert the aggregated data into the new table
+INSERT INTO contact_verified_rules ([rule], valid_count, invalid_count)
+
+--Valid Format
+SELECT 
+	'Valid format' as [rule],
+	COUNT(CASE WHEN TRY_CAST(verified as INT) = 1 OR TRY_CAST(verified as INT) = 0 THEN 1 END) as valid,
+	COUNT(CASE WHEN TRY_CAST(verified as INT) = 1 OR TRY_CAST(verified as INT) = 0 THEN NULL ELSE 1 END) as invalid 
+FROM contact
+UNION
+
+--Email present for Verified Contact Rule
+SELECT 
+	'Email present for verified contact' as [rule],
+	COUNT(CASE WHEN (TRY_CAST(verified as INT) = 1 AND email IS NOT NULL) THEN 1 END) as valid,
+	COUNT(CASE WHEN (TRY_CAST(verified as INT) = 1 AND email IS NULL) THEN 1 END) as invalid 
+FROM contact
+UNION
+
+--Email is Verified
+SELECT 
+	'Email verified' as [rule],
+	COUNT(CASE WHEN TRY_CAST(verified as INT) =1 THEN 1 END) valid,
+	COUNT(CASE WHEN TRY_CAST(verified as INT) != 1 THEN 1 END) invalid
+FROM contact
